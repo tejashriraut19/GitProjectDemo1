0aef45b9b7d6b94311744f939c4e427f0b2dd01c
Shree <Shree@DESKTOP-GFGMQ1C>
2021-11-16 15:32:33 +0530

    first commit

---
+++ src/gitdemo/GitSimpleClass.java
@@ -0,0 +1,20 @@
+package gitdemo;
+
+public class GitSimpleClass {
+	public static void main(String[] args) {
+		
+	
+
+	String hungry="yes";
+	
+	if(hungry=="yes")
+	{
+		System.out.println("eat something");
+	}
+	else
+	{
+		System.out.println("do work");
+	}
+	}
+	
+}
